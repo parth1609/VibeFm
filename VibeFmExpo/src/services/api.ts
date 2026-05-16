a6b1c88be4a9c5c98598b621d2ce383777a6b819
parth patil <parthgajananpatil@gmail.com>
2026-05-16 14:59:07 +0530

    add render url

--- VibeFmExpo/src/services/api.ts
+++ VibeFmExpo/src/services/api.ts
@@ -11,7 +11,7 @@ declare const fetch: any;
 
 const API_BASE_URL = __DEV__
   ? 'http://10.12.139.231:8000'
-  : 'hhttps://vibefm.onrender.com'; // Update this to your production backend URL
+  : 'https://vibefm.onrender.com'; // Update this to your production backend URL
 
 class ApiService {
   private baseUrl: string;
